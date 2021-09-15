ac2f4f35109e442d1f10cc2f09167a795bafb5c4
Sourabh Patil <patilsourabh45@gmail.com>
2021-09-15 11:11:14 +0530

    backend link updated

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 // const BACKEND_URL = 'https://NodewithJWTtoken-Actual.patilsourabh45.repl.co';
-const BACKEND_URL = 'http://localhost:6500';
+const BACKEND_URL = 'https://i50znmkokc.execute-api.us-east-1.amazonaws.com/dev/';
 
 function loginApi(email, password) {
   return axios.get(`${BACKEND_URL}/auth/login`, {
